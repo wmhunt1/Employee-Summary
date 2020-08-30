e589d7633d535858beeccba8efa45dfa642f9013
Matt Hunt <wmatthunt@gmail.com>
2020-08-30 14:16:31 -0400

    need to fix the other question

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,4 +1,5 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
 class Engineer extends Employee {
     constructor(github) {
         super(name,role,email,id);
